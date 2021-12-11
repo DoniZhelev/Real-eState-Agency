294537cee192b34e81e549d8174cb65a9f346629
DoniZhelev <donizhelev@gmail.com>
2021-12-11 15:46:48 +0200

    housing service is almost ready

--- src/controllers/housingController.js
+++ src/controllers/housingController.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const housingService = require('../services/housingService')
 
-router.get('/', (req, res)=>{
-    res.render('housingForRent')
+
+router.get('/', async (req, res)=>{
+    let housings =  await housingService.getAll()
+    res.render('housingForRent',{ housings});
+
 });
 
 router.get('/create', (req, res)=>{
@@ -10,11 +13,30 @@ router.get('/create', (req, res)=>{
 });
 
 router.post('/create', async (req, res) =>{
-    await housingService.create(req.body);
+    await housingService.create({...req.body, owner: req.user._id});
 
 
     res.redirect('/housing')
-})
+});
+
+router.get('/:housingId/details',  async (req,res) =>{
+    let housing = await housingService.getOne(req.params.housingId)
+    
+
+    let isOwner = housing.owner == req.user?._id
+
+    res.render('details', {...housing, isOwner})
+});
+
+    router.get('/:housingId/rent', async (req, res) =>{
+        let housing  = await housingService.getOne(req.params.housingId);
+
+        housing.tenants.push(req.user._id)
+        await housing.save()
+
+        res.redirect(`/housing/${req.params.housingId}/details`);
+
+    })
 
 
 module.exports = router;
